a5f453523285cb583df114951242d41bb950d58d
untername <64744688+untername@users.noreply.github.com>
2020-10-24 14:44:32 +0300

    Update settings.py

--- utils/settings.py
+++ utils/settings.py
@@ -11,11 +11,11 @@ class TokenConfig(BaseSettings):
 class EmailConfig(BaseSettings):
 
     EMAIL_HOST: str = 'smtp.sendgrid.net'
-    EMAIL_HOST_USER: str = 'apikey'
-    EMAIL_HOST_PASSWORD: str = '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'
-    EMAIL_PORT: int = 587
+    EMAIL_HOST_USER: str = ''
+    EMAIL_HOST_PASSWORD: str = ''
+    EMAIL_PORT: int = 0
     EMAIL_USE_TLS: bool = True
-    DEFAULT_FROM_EMAIL: str = 'kalabala971@gmail.com'
+    DEFAULT_FROM_EMAIL: str = ''
 
     HTTP_URL: str = "http://localhost:8000/me"
 
